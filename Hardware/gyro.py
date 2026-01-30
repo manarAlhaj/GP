37f3f1ea6c40bb1a3e156cf60109723b208bb651
Manar Alhaj Yousef <manarmoaied03@gmail.com>
2026-01-30 21:49:59 +0300

    wrote the initial code of reading the flex sensors.

---
+++ Hardware/gyro.py
@@ -0,0 +1,11 @@
+
+# What is the GY-951?
+# The GY-951 is actually a 9-axis IMU (Inertial Measurement Unit) module that contains:
+# Three Sensors in One:
+# 1- Gyroscope (3 axes) - Measures angular velocity (rotation speed)
+# X, Y, Z rotation rates
+# 2- Accelerometer (3 axes) - Measures linear acceleration (and gravity)
+# X, Y, Z acceleration
+# 3- Magnetometer (3 axes) - Measures magnetic field (like a compass)
+# X, Y, Z magnetic field strength
+
