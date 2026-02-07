87cc9d94da5b37ba1392fdfcda362ca828fe189c
Manar Alhaj Yousef <manarmoaied03@gmail.com>
2026-02-07 20:23:48 +0300

    IMUc.py now is written finally as a class. I think it is easier to implement it in a data logger this way

--- Hardware/IMU.py
+++ Hardware/IMU.py
@@ -1,26 +1,33 @@
 from collections import deque
 import math
 import serial
+import time
 
 
 SerialPort = "/dev/serial0"
 BaudRate = 57600
 
-
 PrevRoll = 0
 PrevPitch = 0  
 PrevYaw = 0
 
-
-offset_yaw = 0 
+count = 0
+offset_yaw = 0
+offset_pitch = 0
+offset_roll = 0
 first_time = True
 previous_buffer = deque(maxlen=25)
 
 def get_corrected_yaw(raw_yaw):
-
     return (raw_yaw - offset_yaw) % 360 
 
+def get_corrected_pitch(raw_pitch):
+    return raw_pitch - offset_pitch
 
+def get_corrected_roll(raw_roll):
+    return raw_roll - offset_roll
+
+start = time.time() 
 try:
     ser = serial.Serial(SerialPort, BaudRate, timeout=1)
     ser.reset_input_buffer()
@@ -29,24 +36,30 @@ except Exception as E:
     print("Error establishing serial connection: ", E)
     exit() 
 
-print("Calibrating... Keep sensor STILL.")
+print("Calibrating... Keep sensor STILL and LEVEL.")
+
+time.sleep(0.1)
 
 while True:
     if ser.in_waiting > 0:
-        
         line = ser.readline().decode('utf-8', errors='replace').strip()
         
         if line.startswith("#YPR="):
             data = line.replace("#YPR=", "").split(',')
             if len(data) == 3:
+                offset_yaw = float(data[0])
+                offset_pitch = float(data[1])
+                offset_roll = float(data[2])
                 
-                offset_yaw = float(data[0]) 
-                print(f"Calibration Done! North is set to {offset_yaw}°")
+                print(f"Calibration Done.. ")
+                print(f"  yaw offset: {offset_yaw:.2f} deg ")
+                print(f"  pitch offset: {offset_pitch:.2f} deg")
+                print(f"  roll offset: {offset_roll:.2f} deg ")
                 break
 
 print("Starting Data Stream...")
 
-while True:
+while time.time() - start < 10:
     if ser.in_waiting > 0:
         try:
             line = ser.readline().decode('utf-8', errors='replace').strip()
@@ -54,15 +67,16 @@ while True:
             if line.startswith("#YPR="):
                 data = line.replace("#YPR=", "").split(',')
                 if len(data) == 3:
+                    count = count + 1
                     RawYaw   = float(data[0])
                     RawPitch = float(data[1])
                     RawRoll  = float(data[2])
 
+        
                     CurrentYaw = get_corrected_yaw(RawYaw)
-                    CurrentPitch = RawPitch
-                    CurrentRoll = RawRoll
+                    CurrentPitch = get_corrected_pitch(RawPitch)
+                    CurrentRoll = get_corrected_roll(RawRoll)
 
-                 
                     if first_time: 
                         DeltaYaw, DeltaPitch, DeltaRoll = 0, 0, 0
                         first_time = False
@@ -71,23 +85,27 @@ while True:
                         DeltaPitch = CurrentPitch - PrevPitch
                         DeltaRoll = CurrentRoll - PrevRoll
 
-                        
+                        #yaw wraparound bc u fking technically can move around yourself in a circle (360)
+                        #pitch and roll of your hand wont rotate over 180 unless u break ur wrist ok 
                         if DeltaYaw > 180: 
                             DeltaYaw -= 360
                         if DeltaYaw < -180:
                             DeltaYaw += 360
 
+                   
                     YawRadians = math.radians(CurrentYaw)
-                    YawSine = round(math.sin(YawRadians),3)
-                    YawCosine = round(math.cos(YawRadians),3)
+                    PitchRadians = math.radians(CurrentPitch)
+                    RollRadians = math.radians(CurrentRoll)
 
                     features = [
-                        YawSine, 
-                        YawCosine,
-                        round(CurrentPitch, 2), 
-                        round(CurrentRoll, 2), 
-                        round(DeltaYaw, 2), 
-                        round(DeltaPitch, 2), 
+                        round(math.sin(YawRadians), 3),
+                        round(math.cos(YawRadians), 3),
+                        round(math.sin(PitchRadians), 3),
+                        round(math.cos(PitchRadians), 3),
+                        round(math.sin(RollRadians), 3),
+                        round(math.cos(RollRadians), 3),
+                        round(DeltaYaw, 2),
+                        round(DeltaPitch, 2),
                         round(DeltaRoll, 2)
                     ]
 
@@ -103,3 +121,6 @@ while True:
             pass 
         except Exception as e:
             print(f"Error: {e}")
+
+print(count)
+print(count/10, " hz")
